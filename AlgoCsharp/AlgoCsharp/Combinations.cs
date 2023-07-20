35ab33d24fb1bb06a11c9de98db4ad30465e9119
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-20 10:25:23 +0200

    permutations

---
+++ AlgoCsharp/AlgoCsharp/Combinations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace AlgoCsharp
+{
+	public class Combinations
+	{
+
+		public Combinations()
+		{
+
+		}
+
+
+        // Function to calculate factorial (n!)
+       public static int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+
+        // Function to calculate the number of permutations (P(n, r))
+        public static int Permutations(int n, int r)
+        {
+            int numerator = Factorial(n);
+            int denominator = Factorial(n - r);
+            return numerator / denominator;
+        }
+
+    }
+}
---
+++ AlgoCsharp/AlgoCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgoCsharp
+{
+    class Program
+    {
+      
+        static void Main()
+        {
+             char[] characters = { 'A', 'B', 'C', 'D' };
+
+           int perm= Combinations.Permutations(characters.Length,2);
+            Console.WriteLine(perm);
+
+
+
+        }
+    }
+}
